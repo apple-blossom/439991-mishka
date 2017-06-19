cd681a7d68f6d008cde8ee707c79546e561d9667
apple-blossom <beep-93@mail.ru>
2017-06-19 13:13:27 +0300

    add js for modal form, add hovers that are not working

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
  window.onload = function(){ 
  
-    var button = document.getElementById('menu-button');
+    var buttonMenu = document.getElementById('menu-button');
     var menuTop = document.querySelector('.site-header__nav-item-container-top');
     var menuBottom = document.querySelector('.site-header__nav-item-container-bottom');
 
-    button.onclick = function() {
+    buttonMenu.onclick = function() {
       menuTop.classList.toggle('site-header__nav-item-container-top--closed');
       menuBottom.classList.toggle('site-header__nav-item-container-bottom--closed');
+      buttonMenu.classList.toggle('site-header__button--open');
     };
+
+    var buttonBuy = document.getElementById('buy');
+    var modalForm = document.querySelector('.buy-form');
+
+    buttonBuy.onclick = function() {
+      modalForm.classList.toggle('buy-form--open');
+    };
+
 }; 
